543253a8a1c4357131adef75cdaf8f612568a613
Joeltro <joeltro.junior@gmail.com>
2025-11-13 23:33:07 -0300

    Mudando main
    Simplifica bibliotecas e mudando ultima data de atualizacao

--- main.c
+++ main.c
@@ -1,12 +1,10 @@
 /*----------------------------------------------------------------------
 Conversor de unidades 
 Dupla: Alisson e Joeltro
-Data da ultima modificação: 11/11/2025
+Data da ultima modificação: 13/11/2025
 ------------------------------------------------------------------------
 */
 
-#include <stdio.h>
-#include <stdlib.h>
 #include "func.h"
 
 int main(void) {
