82d6f2f6e6dc2f793cecbaaf796cb1f7d0d4ffe6
Joeltro <joeltro.junior@gmail.com>
2025-12-01 11:14:10 -0300

    Aumenta tamanho do commando
    Solucao temporaria, implementar malloc depois

--- func.h
+++ func.h
@@ -6,7 +6,7 @@
 #include <signal.h>
 
 typedef struct celula { // struct que será utilizada para fazer a lista do programa
-    char command[100]; // são os comandos digitados pelo usúario
+    char command[303]; // são os comandos digitados pelo usúario
     struct celula* ant; // ponteiro que irá apontar para o comando anterior(dado)
     struct celula* prox; // ponteiro que irá apontar para o próximo
 } celula;
