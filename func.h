17910b3cc018f30a8314559ebf5440e793ce9695
Joeltro <joeltro.junior@gmail.com>
2025-12-02 11:36:51 -0300

    Comando alocado dinamicamente

--- func.h
+++ func.h
@@ -1,13 +1,10 @@
 #pragma once
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <string.h>
-#include <signal.h>
 
 typedef struct celula { // struct que será utilizada para fazer a lista do programa
-    char command[303]; // são os comandos digitados pelo usúario
-    struct celula* ant; // ponteiro que irá apontar para o comando anterior(dado)
+    char* command; // são os comandos digitados pelo usúario
     struct celula* prox; // ponteiro que irá apontar para o próximo
 } celula;
 
